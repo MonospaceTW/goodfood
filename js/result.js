e8dd38c40d36a362046622c120330d4bcd41f0b9
raindot <sakuhime24@gmail.com>
2017-12-18 17:27:20 +0800

    render orders

---
+++ js/result.js
@@ -0,0 +1,15 @@
+new Vue({
+  el:'#result',
+  data: {
+    items:[
+      {dishName:'炒飯',number:1,subtotal:60},
+      {dishName:'鍋貼',number:2,subtotal:120},
+    ],
+    orders:[
+      {userID:'Clover',iorders:[
+        {dishName:'炒飯',number:1,subtotal:60},
+        {dishName:'鍋貼',number:2,subtotal:120}
+      ]}
+    ]
+  }
+});
