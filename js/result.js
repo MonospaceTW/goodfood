deb1f2f7faef78c374d99d4dcddb2f2b49fb4894
raindot <sakuhime24@gmail.com>
2017-12-19 15:39:43 +0800

    計算總訂單ing

--- js/result.js
+++ js/result.js
@@ -1,35 +1,50 @@
 new Vue({
-  el:'#result',
+  el: '#result',
   data: {
-    items:[
-      {dishName:'炒飯',number:1,subtotal:60},
-      {dishName:'鍋貼',number:2,subtotal:120},
+    items: [
+      { dishName: '炒飯', number: 1, subtotal: 60 },
+      { dishName: '鍋貼', number: 2, subtotal: 120 },
     ],
-    orders:[
-      {userID:'Clover',
-      order:[
-        {dishName:'炒飯',number:1,subtotal:60},
-        {dishName:'鍋貼',number:2,subtotal:120}
-      ]
-    },
-    {userID:'Kent',
-    order:[
-      {dishName:'炒飯',number:1,subtotal:60},
-      {dishName:'鍋貼',number:2,subtotal:120}
-    ]
-  }
-        
+    orders: [
+      {
+        userID: 0,
+        name: 'Clover',
+        order: [
+          { dishName: '炒飯', number: 1, subtotal: 60 },
+          { dishName: '鍋貼', number: 2, subtotal: 120 }
+        ]
+      },
+      {
+        userID: 1,
+        name: 'Kent',
+        order: [
+          { dishName: '炒飯', number: 1, subtotal: 60 },
+          { dishName: '水餃', number: 3, subtotal: 120 }
+        ]
+      }
+
     ]
   },
-  computed:{
-    total(){
-      let total=0;
-      for (let i=0; i<this.items.length; i++){
-        total+= this.items[i].subtotal;
+  computed: {
+    total() {
+      let total = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        total += this.items[i].subtotal;
       }
       return total;
+    },
+    totalOrders() {
+      let items = [];
+      let item = {};
+      for (let i = 0; i < this.orders.length; i++) {
+        item.dishName = this.orders[i].order[i].dishName;
+        item.number = this.orders[i].order[i].number;
+        items.push(item);
+      }
+
+      return items;
     }
   }
-  
+
 });
 
