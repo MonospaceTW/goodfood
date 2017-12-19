8fb970802fff5f8914da914a7196280e3239208c
raindot <sakuhime24@gmail.com>
2017-12-19 17:43:44 +0800

    計算總訂單

--- js/result.js
+++ js/result.js
@@ -1,29 +1,44 @@
+// 詳細訂單資料
+const orders = [
+  {
+    userID: 0,
+    name: 'Clover',
+    order: [
+      { dishName: '炒飯', number: 1, subtotal: 60 },
+      { dishName: '鍋貼', number: 2, subtotal: 120 }
+    ]
+  },
+  {
+    userID: 1,
+    name: 'Kent',
+    order: [
+      { dishName: '炒飯', number: 2, subtotal: 120 },
+      { dishName: '水餃', number: 3, subtotal: 120 }
+    ]
+  }
+
+];
+
+// 產生總訂單
+let computedOrders = [];
+for (let i = 0; i < orders.length; i++) {
+  for (let j = 0; j < orders[i].order.length; j++) {
+    let item = {};
+    item.dishName = orders[i].order[j].dishName;
+    item.number = orders[i].order[j].number;
+    computedOrders.push(item);
+  }
+}
+console.log(computedOrders);
+
+
+
+
 new Vue({
   el: '#result',
   data: {
-    items: [
-      { dishName: '炒飯', number: 1, subtotal: 60 },
-      { dishName: '鍋貼', number: 2, subtotal: 120 },
-    ],
-    orders: [
-      {
-        userID: 0,
-        name: 'Clover',
-        order: [
-          { dishName: '炒飯', number: 1, subtotal: 60 },
-          { dishName: '鍋貼', number: 2, subtotal: 120 }
-        ]
-      },
-      {
-        userID: 1,
-        name: 'Kent',
-        order: [
-          { dishName: '炒飯', number: 1, subtotal: 60 },
-          { dishName: '水餃', number: 3, subtotal: 120 }
-        ]
-      }
-
-    ]
+    items: computedOrders,
+    orders: orders
   },
   computed: {
     total() {
@@ -33,18 +48,11 @@ new Vue({
       }
       return total;
     },
-    totalOrders() {
-      let items = [];
-      let item = {};
-      for (let i = 0; i < this.orders.length; i++) {
-        item.dishName = this.orders[i].order[i].dishName;
-        item.number = this.orders[i].order[i].number;
-        items.push(item);
-      }
 
-      return items;
-    }
   }
 
 });
+
+
+
 
