d7b24f3ceca8b1f89ebc74fdb13b21897da97db2
raindot <sakuhime24@gmail.com>
2018-01-15 16:52:43 +0800

    套用輪播

--- js/index.js
+++ js/index.js
@@ -1,26 +1,16 @@
-Vue.component('my-component', {
-  template: '#my-component',
-  data: function () {
-    return {
-      imglink1: 'img/c.png',
-      imglink2: 'img/u.png',
-      imglink3: 'img/j.png',
-      imglink4: 'img/e.png',
-      imglink5: 'img/l.png',
-      imglink6: 'img/d.png',
-
-    }
-  },
-  methods: {
-    search() {
-      console.log(1);
-    }
-  }
-});
-
 const vm = new Vue({
-  el: '#app',
+  el: '#index',
   data: {
 
   }
 })
+
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
