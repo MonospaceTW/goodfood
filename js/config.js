6ca42dbc7630f0c55e2d5daa1a68dbe54bbae955
horsekitlin <horsekit1982@gmail.com>
2018-01-21 01:15:20 +0800

    reset to master

---
+++ js/config.js
@@ -0,0 +1,10 @@
+
+var config = {
+  apiKey: "AIzaSyBMRANtWtuf5Quz26GxkEnb4rRJ1t7OAkg",
+  authDomain: "goodfood-39158.firebaseapp.com",
+  databaseURL: "https://goodfood-39158.firebaseio.com",
+  projectId: "goodfood-39158",
+  storageBucket: "goodfood-39158.appspot.com",
+  messagingSenderId: "940303768399"
+};
+firebase.initializeApp(config);
