1ce0b37808d3c13d5a3e14c0a3a78b3aae321a14
horsekitlin <horsekit1982@gmail.com>
2018-01-21 01:09:31 +0800

    Revert "Tomas"

--- docs/tomas/git/pullrequest.md
+++
@@ -1,7 +0,0 @@
-# 關於發 PR(Pull Request)
-
-[github](https://help.github.com/articles/about-pull-requests/)
-
-[github 多人共同開發專案相關介紹](https://git-scm.com/book/zh-tw/v2/GitHub-%E5%8F%83%E8%88%87%E4%B8%80%E5%80%8B%E5%B0%88%E6%A1%88)
-
-[Pull Request 的管理與解釋](http://www.ruanyifeng.com/blog/2017/07/pull_request.html)
---
+++ js/index.js
@@ -0,0 +1,86 @@
+
+
+firebase.database().ref('store').once('value').then(function (snapshot) {
+  // 店家列表
+  var stores = [];
+  var store = {};
+  snapshot.forEach(function (data) {
+    store = data.val();
+    store.key = data.key;
+    stores.push(store);
+  });
+  // console.log(stores);
+
+  // 抽出首頁要推薦的n個店家
+  var recommendNum = 5;
+  var recommend = randomStore(stores, recommendNum);
+  vm.update(recommend);
+  console.log(recommend);
+
+  // 抽出好手氣店家
+  var lottery = randomStore(stores, 1);
+  console.log(lottery);
+  vm.lottery = lottery.pop();
+});
+
+// 隨機抽出n個不重複的項目
+function randomStore(rawData, count) {
+  var result = [];
+  var data = rawData.slice(0);
+  console.log(data);
+  for (var i = 0; i < count; i++) {
+    var ran = Math.floor(Math.random() * data.length);
+    result.push(data.splice(ran, 1)[0]);
+  };
+  return result;
+}
+
+Vue.use(window.VueAwesomeSwiper);
+
+const vm = new Vue({
+  el: '#index',
+  data: {
+    recommend: [],
+    lottery: "",
+    // swiper的選項
+    swiperOption: {
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: true
+      },
+      loop: true
+    }
+  },
+  components: {
+    LocalSwiper: VueAwesomeSwiper.swiper,
+    LocalSlide: VueAwesomeSwiper.swiperSlide,
+  },
+  computed: {
+    // swiperB() {
+    //   return this.$refs.storeSwiper.swiper
+    // }
+  },
+  methods: {
+    update(recommend) {
+      this.recommend = recommend;
+      // console.log(this.recommend);
+    },
+    goRecommend(store) {
+      location.href = "menu.html?storeId=" + store.key;
+    },
+    goodLuck() {
+      if (this.lottery) {
+        location.href = "menu.html?storeId=" + this.lottery.key;
+      }
+    }
+  }
+})
+
