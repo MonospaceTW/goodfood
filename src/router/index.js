7001d42d846a6a2d4460d1ebc3e59391f883798f
Raindot <sakuhime24@gmail.com>
2018-01-23 19:38:56 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import todoList from "@/components/todoList";
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/todo',
+      name: 'todoList',
+      component: todoList
+    },
   ]
 })
