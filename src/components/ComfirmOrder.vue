10f1da956de4787a4bbbb7eda858a8c77a30de43
Raindot <sakuhime24@gmail.com>
2018-01-29 00:43:39 +0800

    測試router

---
+++ src/components/ComfirmOrder.vue
@@ -0,0 +1,14 @@
+<script>
+var firebase = require("firebase");
+
+export default {
+  props: ["orderId", "storeId"]
+};
+</script>
+
+<template>
+  <div>
+    {{this.orderId}}
+    {{this.storeId}}
+  </div>
+</template>
