373ebbec09aff2ea08c437aa600f0cfc87633a3e
Raindot <sakuhime24@gmail.com>
2018-01-29 15:48:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <router-link to="/HelloWorld">HelloWorld</router-link>
-    <router-link to="/todo">todo list</router-link>    
     <router-view/>
   </div>
 </template>
@@ -14,25 +12,5 @@ export default {
 </script>
 
 <style lang="scss">
-// @import "../node_modules/susy/sass/susy";
 
-// $susy: (
-//   columns: 12,
-//   container: 1140px,
-//   gutters: 0.25,
-//   global-box-sizing: border-box,
-//   debug: (image: show-columns)
-// );
-// .testing {
-//   @include span(12);
-// }
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
