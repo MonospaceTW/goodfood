38cd023d5bd990b4b26b3d58c920b2554eecf79f
Raindot <sakuhime24@gmail.com>
2018-02-03 14:12:13 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -4,46 +4,46 @@ import Router from "vue-router";
 import storeinfo from "../components/storeInfo";
 import App from "@/App";
 import Order from "@/components/Order";
-import ComfirmOrder from "@/components/ComfirmOrder";
+import Comfirmed from "@/components/Comfirmed";
 import NotFound from "@/components/NotFound";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-
     {
       path: "/",
       name: "App",
       component: App,
-      children: [{
-        path: "storeinfo/:store_id?",
-        name: "storeinfo",
-        component: storeinfo,
-        props: true
-      },
-      {
-        path: "order/:store_id?/:order_id?",
-        name: "order",
-        component: Order,
-        props: true
-      },
-      {
-        path: "comfirm_order",
-        name: "comfirm_order",
-        component: ComfirmOrder
-        // props: true
-      },
-      {
-        path: "result",
-        name: "result",
-        component: Result
-      },
-      {
-        path: "not_found",
-        name: "not_found",
-        component: NotFound
-      }
+      children: [
+        {
+          path: "storeinfo/:store_id?",
+          name: "storeinfo",
+          component: storeinfo,
+          props: true
+        },
+        {
+          path: "order/:store_id?/:order_id?",
+          name: "order",
+          component: Order,
+          props: true
+        },
+        {
+          path: "comfirmed",
+          name: "comfirmed",
+          component: Comfirmed
+          // props: true
+        },
+        {
+          path: "result",
+          name: "result",
+          component: Result
+        },
+        {
+          path: "not_found",
+          name: "not_found",
+          component: NotFound
+        }
       ]
     },
     {
