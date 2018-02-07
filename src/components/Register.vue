cd11d334f9eea0d92734a5a95066a3d47d505e86
Raindot <sakuhime24@gmail.com>
2018-02-07 23:55:03 +0800

    註冊新用戶與表單驗證

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,60 +1,106 @@
 <script>
 var firebase = require("firebase");
+var SimpleVueValidation = require("simple-vue-validator");
+var Validator = SimpleVueValidation.Validator.create({
+  templates: {
+    required: "此欄位為必填",
+    minLength: "密碼長度需大於六字元",
+    email: "e-mail格式不符",
+    match: "密碼不一致"
+  }
+});
 
 export default {
   data() {
     return {
       email: "",
-      name: "",
-      pw: "",
-      comfirmPw: ""
+      displayName: "",
+      password: "",
+      repeat: "",
+      submitted: false,
+      errorMsg: ""
     };
   },
-  created() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        console.log("User is signed in.", user);
-      } else {
-        console.log("User is not logined yet.");
+  mixins: [require("simple-vue-validator").mixin],
+  validators: {
+    email: function(value) {
+      return Validator.value(value)
+        .required()
+        .email();
+    },
+    displayName: function(value) {
+      return Validator.value(value).required();
+    },
+    password: function(value) {
+      return Validator.value(value)
+        .required()
+        .minLength(6);
+    },
+    "repeat, password": function(repeat, password) {
+      if (this.submitted || this.validation.isTouched("repeat")) {
+        return Validator.value(repeat)
+          .required()
+          .match(password);
       }
-    });
+    }
+  },
+  created() {
+    // firebase.auth().onAuthStateChanged(function(user) {
+    //   if (user) {
+    //     // User is signed in.
+    //     console.log("User is signed in.", user);
+    //   } else {
+    //     console.log("User is not logined yet.");
+    //   }
+    // });
   },
   methods: {
     register() {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(this.email, this.pw)
-        .then(function() {
-          console.log("sucess");
-        })
-        .catch(function(error) {
-          // Handle Errors here.
+      this.submitted = true;
+      this.$validate().then(success => {
+        if (success) {
+          console.log("Validation succeeded!", this.email, this.password);
+          let email = this.email;
+          let password = this.password;
+          let displayName = this.displayName;
+          firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(user => {
+              console.log("sucess", user);
 
-          console.log(error.message, error.code);
-          switch (error.message) {
-            case "The password must be 6 characters long or more.":
-              console.log("密碼需多於六個字元");
-              break;
-            case "Password should be at least 6 characters":
-              console.log("密碼需多於六個字元");
-              break;
-            case "The email address is already in use by another account.":
-              console.log("此帳號已被註冊");
-              break;
-          }
-        });
-    },
-    signOut() {
-      firebase
-        .auth()
-        .signOut()
-        .then(function() {
-          // Sign-out successful.
-        })
-        .catch(function(error) {
-          // An error happened.
-        });
+              user
+                .updateProfile({
+                  displayName: displayName
+                })
+                .then(function() {
+                  console.log("update displayName sucess", user.displayName);
+                })
+                .catch(function(error) {
+                  console.log(error.message);
+                });
+              this.$router.go(-2);
+            })
+            .catch(error => {
+              console.log(error.message, error.code);
+              switch (error.code) {
+                case "auth/weak-password":
+                  this.errorMsg = "密碼需多於六個字元";
+                  break;
+                case "auth/invalid-email":
+                  this.errorMsg = "E-mail格式錯誤";
+                  break;
+                case "auth/email-already-in-use":
+                  console.log("此帳號已被註冊", this.errorMsg);
+                  this.errorMsg = "此帳號已被註冊";
+                  break;
+                case "auth/operation-not-allowed":
+                  this.errorMsg = "現在不開放註冊";
+                  break;
+              }
+            });
+        }
+      });
     }
   }
 };
@@ -63,18 +109,43 @@ export default {
 <template>
   <div class="container">
     <h1>會員註冊</h1>
-    <form class="register_form" action="" @submit.prevent="register">
-      <label for="email">E-mail</label>
-      <input type="email" id="email" placeholder="example@gmail.com" v-model="email">
-      <label for="name">暱稱</label>
-      <input type="text" id="name" placeholder="請輸入暱稱" v-model="name">
-      <label for="pw">密碼</label>
-      <input type="password" id="pw" placeholder="密碼長度需多於6字元" v-model="pw">
-      <label for="comfirm_pw">確認密碼</label>
-      <input type="password" id="comfirm_pw" placeholder="請再次輸入密碼" v-model="comfirmPw">
-      <button type="submit">送出</button>
-    </form>
-    <button @click="signOut">登出</button>
+    <div class="layout-form">
+  <form action="" @submit.prevent="register">
+    <div class="form-group" :class="{error: validation.hasError('email')}">
+      <div class="label">E-mail</div>
+      <div class="content">
+        <input type="email" class="form-control" v-model="email" />
+      </div>
+      <div class="message">{{ validation.firstError('email') }}{{errorMsg}}</div>
+    </div>
+    <div class="form-group" >
+      <div class="label">暱稱</div>
+      <div class="content">
+        <input type="text" class="form-control" v-model="displayName" />
+      </div>
+      <div class="message">{{ validation.firstError('displayName') }}</div>
+    </div>
+    <div class="form-group" :class="{error: validation.hasError('password')}">
+      <div class="label">密碼</div>
+      <div class="content">
+        <input type="password" class="form-control" v-model="password" />
+      </div>
+      <div class="message">{{ validation.firstError('password') }}</div>
+    </div>
+    <div class="form-group" :class="{error: validation.hasError('repeat')}">
+      <div class="label">確認密碼</div>
+      <div class="content">
+        <input type="password" class="form-control" v-model="repeat" />
+      </div>
+      <div class="message">{{ validation.firstError('repeat') }}</div>
+    </div>
+    <div class="form-group">
+      <div class="actions">
+        <button type="submit" class="btn btn-primary">送出</button>
+      </div>
+    </div>
+  </form>
+</div>
   </div>
 </template>
 
