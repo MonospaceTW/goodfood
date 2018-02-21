f04103375651304edc00e56d40dbecb37547c05c
Raindot <sakuhime24@gmail.com>
2018-02-21 15:27:55 +0800

    拉皮

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -3,7 +3,7 @@
 import FirebaseManager from "@/utils/FirebaseManager";
 import ComfirmOrder from "./ComfirmOrder";
 import checkAuth from "@/checkAuth";
-import fp from "lodash/fp";
+import lodashfp from "lodash/fp";
 
 export default {
   props: ["storeId", "orderId"],
@@ -41,7 +41,7 @@ export default {
         menu[id].count = 0;
       }
       //  深層複製物件以確保更新Vue的observer
-      this.dishes = fp.cloneDeep(menu);
+      this.dishes = lodashfp.cloneDeep(menu);
       console.log(this.dishes);
     });
   },
@@ -107,47 +107,61 @@ export default {
 <div class="container">
   <comfirm-order v-show="comfirmed" :user="user" :orderId="orderId" :storeId="storeId" :uid="uid" @cancelOrder="cancelOrder"></comfirm-order>
   <h1 class="store_name">便當店名</h1>
-    <ul>
-      <li class="item" v-for="(dish,id) in dishes" v-bind:key="id">
-        <div class="menu">
-          <div class="menu_name">{{dish.name}}</div>
-          <div class="price">${{dish.price}}</div>
+  <ul>
+    <li class="item" v-for="(dish,id) in dishes" v-bind:key="id">
+      <div class="menu">
+        <div class="menu_name">{{dish.name}}</div>
+        <div class="price">${{dish.price}}</div>
+      </div>
+      <div class="menu">
+        <div class="counter">
+          <button class="subtract" @click="subtract(id)">-</button>
+          <div class="count">{{dish.count}}</div>
+          <button class="add" @click="add(id)">+</button>
         </div>
-        <div class="menu">
-          <div class="counter">
-            <button @click="subtract(id)">-</button>
-            <div>{{dish.count}}</div>
-            <button @click="add(id)">+</button>
-          </div>
-          <div class="subtotal">${{subtotal(id)}}</div>
-        </div>
-      </li>
-    </ul>
- <div class="total">總共{{total}}元</div>
-<div>
-  我是{{displayName}}
-</div>
-<div>
-  備註：<input type="text" v-model="mark"></div>
-  <a class="order_btn" href="#" @click="order">下訂單</a>
-  <a class="result_btn" href="#">看團定結果</a>
-  <a class="share_btn" href="#">分享這頁</a>
+        <div class="subtotal">${{subtotal(id)}}</div>
+      </div>
+    </li>
+  </ul>
+  <div class="total">
+    <span>總計</span>＄{{total}}</div>
+  <div class="name">
+    我是{{displayName}}
+  </div>
+  <div class="btn_group">
+    <!-- 備註：<input type="text" v-model="mark"></div> -->
+    <a class="order_btn" href="#" @click="order">下訂單</a>
+    <a class="result_btn" href="#">看團定結果</a>
+    <a class="share_btn" href="#">分享這頁</a>
+  </div>
 </div>
 </template>
 
 
 <style lang="scss" scoped>
+@import "../scss/index.scss";
+
+a {
+  color: $orange;
+  text-decoration: none;
+}
+
 li {
   list-style: none;
 }
 
 .container {
   width: 90%;
+  font-size: 14px;
   margin: 0 auto;
+  padding-top: 60px;
+  padding-bottom: 60px;
   position: relative;
 }
 
 .store_name {
+  margin-bottom: 24px;
+  font-size: 17px;
   text-align: center;
 }
 
@@ -155,33 +169,60 @@ li {
   padding: 10px;
   margin: 10px 0;
   border-radius: 15px;
-  background-color: rgb(223, 222, 222);
+  background-color: $gray_one;
 }
 
 .menu,
 .counter {
+  padding: 6px 6px 6px 0;
   display: flex;
   justify-content: space-between;
+}
+
+.subtract,
+.add {
+  width: 20px;
+  height: 23px;
+  padding-right: 15px;
+  color: $orange;
+  border: $orange 1px solid;
+  border-radius: 50%;
+  background-color: $gray_one;
+}
+
+.count {
+  padding: 0 8px;
+  margin: 0 10px;
+  line-height: 20px;
+  background-color: white;
+}
+
+.btn_group {
+  display: flex;
+  justify-content: space-around;
 }
 
 .order_btn,
 .result_btn,
 .share_btn {
   display: block;
-  margin: 0 auto;
-  width: 200px;
+  margin: 0 6px;
+  width: 100px;
   height: 50px;
   line-height: 50px;
   text-align: center;
-  border: 1px orange solid;
-  border-radius: 20px;
+  border: 1px $orange solid;
+  border-radius: 30px;
 }
 
 .total {
   text-align: right;
+  span {
+    color: $blue;
+  }
 }
 
-.order_btn {
-  background-color: orange;
+.name {
+  padding: 20px 0;
 }
 </style>
