6a521aad48ed6d94c035770dd2af36ff6361cd3e
Raindot <sakuhime24@gmail.com>
2018-03-02 01:14:49 +0800

    storelist/調整router-link&CSS

--- src/components/StoreList.vue
+++ src/components/StoreList.vue
@@ -45,12 +45,11 @@ export default {
 </script>
 <template>
 <div class="container">
-  <header></header>
   <h1 class="list_title">合作店家</h1>
   <div class="content">
     <ul>
       <li v-for="(value, key) in stores" :key="key.id">
-        <router-link :to="{path:'/order/'+key}">
+        <router-link :to="{path:'/storeinfo/'+key}">
           <img src="https://fakeimg.pl/128x90/?text=Food&font=lobster" alt="">
         </router-link>
         <div class="info_box">
@@ -70,141 +69,10 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-html,
-body,
-div,
-span,
-applet,
-object,
-iframe,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p,
-blockquote,
-pre,
-a,
-abbr,
-acronym,
-address,
-big,
-cite,
-code,
-del,
-dfn,
-em,
-img,
-ins,
-kbd,
-q,
-s,
-samp,
-small,
-strike,
-strong,
-sub,
-sup,
-tt,
-var,
-b,
-u,
-i,
-center,
-dl,
-dt,
-dd,
-ol,
-ul,
-li,
-fieldset,
-form,
-label,
-legend,
-table,
-caption,
-tbody,
-tfoot,
-thead,
-tr,
-th,
-td,
-article,
-aside,
-canvas,
-details,
-embed,
-figure,
-figcaption,
-footer,
-header,
-hgroup,
-menu,
-nav,
-output,
-ruby,
-section,
-summary,
-time,
-mark,
-audio,
-video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article,
-aside,
-details,
-figcaption,
-figure,
-footer,
-header,
-hgroup,
-menu,
-nav,
-section {
-  display: block;
-}
-body {
-  line-height: 1;
-}
-ol,
-ul {
-  list-style: none;
-}
-blockquote,
-q {
-  quotes: none;
-}
-blockquote:before,
-blockquote:after,
-q:before,
-q:after {
-  content: "";
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
 /*至此結束Reset*/
 
-header {
-  width: 375px;
-  height: 20px;
-  background: #000;
-}
 .container {
-  width: 375px;
   height: 667px;
-  border: 1px solid red;
   margin: 0 auto;
 }
 .list_title {
