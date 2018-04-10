d4185f1bf090d4740d3d90c4408ca410cb08bd11
yingming25 <karcnonf@gmail.com>
2018-04-10 15:14:27 +0800

    Update adminmenu component

--- src/components/AdminMenu.vue
+++ src/components/AdminMenu.vue
@@ -1,10 +1,165 @@
 <template>
-  
+  <div class="container">
+      <div class="menus">
+        <ul>
+          <li v-for="(menu, key) in menus" :key="key.id">
+            <div class="menu-align-area1">
+              <a href="#" class="remove" @click.prevent="removeMenu($event, key)">
+                <span class="remove-inner"></span>
+              </a>
+              <div class="dishName">{{menu.name}}</div>
+            </div>
+            <div class="dishPrice">${{menu.price}}</div>
+          </li>
+        </ul>
+
+        <div class="check-bar">
+          <a href="#" class="add-menu">新增菜單</a>
+          <div class="select-area">
+            <a href="#">
+              <img class="cancel" src="../assets/images/cancel.png" alt="">
+            </a>
+            <a href="#">
+              <img class="check" src="../assets/images/check.png" alt="">
+            </a>
+          </div>
+        </div>
+
+      </div>
+  </div>
 </template>
 <script>
+import FirebaseManager from "@/utils/FirebaseManager";
+
+const store = FirebaseManager.database.ref("store");
+
+export default {
+  props: ["store-id"],
+  data() {
+    return {
+      menus: {},
+      removeMenus: []
+    };
+  },
+  created() {
+    const self = this;
+    const storeId = self.storeId;
+    /* 
+      將店家的菜單加入 data 中的 menus 中，
+      以便在 template 中使用 v-for 渲染菜單列表。 
+    */
+    store
+      .child(storeId)
+      .child("menus")
+      .once("value")
+      .then(snapshot => {
+        // console.log(snapshot.val());
+        // snapshot.forEach(data => {
+        // this.menus.push(data.val());
+        // });
+        this.menus = snapshot.val();
+        console.log(snapshot.val());
+      });
+    // console.log(self.menus);
+
+    self.removeMenus = JSON.parse(localStorage.getItem("removeMenuKeys"));
+  }
+};
 </script>
-
 <style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 405px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 5px;
+}
+.menus {
+  width: 84.269%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.17);
+  border-radius: 14px;
+  background: #ffffff;
+  padding: 30px 7px 0 17px;
+  ul {
+    width: 100%;
+    height: 300px;
+    // align-self: center;
+    overflow: auto;
+    li {
+      height: 49px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid #e9e9e9;
+    }
+  }
+}
 
+.menu-align-area1 {
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+}
+
+.remove {
+  width: 19px;
+  height: 19px;
+  border-radius: 50%;
+  border: solid 1px #f75454;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 0;
+}
+
+.remove-inner {
+  width: 10px;
+  height: 2px;
+  background-color: #f75454;
+}
+
+.dishName {
+  height: 20px;
+  line-height: 20px;
+  display: inline-block;
+  margin-left: 13px;
+}
+
+.dishPrice {
+  height: 20px;
+  line-height: 20px;
+}
+
+.check-bar {
+  display: flex;
+  width: 100%;
+  height: 55px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.add-menu {
+  color: #f8a654;
+  text-decoration: none;
+  margin-left: 25px;
+}
+
+.check {
+  margin-left: 30px;
+}
+
+.select-area {
+  margin-right: 35px;
+}
+
+p {
+  margin-top: 0;
+}
 </style>
 
--- src/components/AdminStoreInfo.vue
+++ src/components/AdminStoreInfo.vue
@@ -2,34 +2,7 @@
   <div class="container" :style="{backgroundImage:`url(${img})`}">
     <div class="content">
       <h1>{{storeInfo.name}}</h1>
-
-      <div class="menus">
-        <ul>
-          <li v-for="(menu, key) in menus" :key="key.id">
-            <div class="menu-align-area1">
-              <a href="#" class="remove" @click.prevent="removeMenu($event, key)">
-                <span class="remove-inner"></span>
-              </a>
-              <div class="dishName">{{menu.name}}</div>
-            </div>
-            <div class="dishPrice">${{menu.price}}</div>
-          </li>
-        </ul>
-
-        <div class="check-bar">
-          <a href="#" class="add-menu">新增菜單</a>
-          <div class="select-area">
-            <a href="#">
-              <img class="cancel" src="../assets/images/cancel.png" alt="">
-            </a>
-            <a href="#">
-              <img class="check" src="../assets/images/check.png" alt="">
-            </a>
-          </div>
-        </div>
-
-      </div>
-
+      <admin-menu :store-id="parentStoreId"></admin-menu>
     </div>
     <footer-component></footer-component>
   </div>
@@ -38,6 +11,7 @@
 <script>
 import FirebaseManager from "@/utils/FirebaseManager";
 import checkAuth from "@/checkAuth";
+import adminMenu from "./AdminMenu";
 import footerComponent from "./Footer";
 import axios from "axios";
 
@@ -50,6 +24,7 @@ const order = FirebaseManager.database.ref("order");
 export default {
   props: ["storeId"],
   components: {
+    adminMenu,
     footerComponent
   },
   data() {
@@ -74,12 +49,13 @@ export default {
         endTime: ""
       },
       storeInfoAll: [],
-      menus: {},
-      removeMenus: []
+      parentStoreId: this.$props.storeId
     };
   },
   created() {
-    const storeId = this.storeId;
+    // const self = this;
+    // const storeId = self.storeId;
+    // console.log(this.parentStoreId);
 
     checkAuth
       .checkAuth()
@@ -105,25 +81,6 @@ export default {
     //     this.storeInfo = snapshot.val();
     //     // console.log(self.storeInfo);
     //   });
-
-    /* 
-      將店家的菜單加入 data 中的 menus 中，
-      以便在 template 中使用 v-for 渲染菜單列表。 
-    */
-    store
-      .child(storeId)
-      .child("menus")
-      .once("value")
-      .then(snapshot => {
-        // console.log(snapshot.val());
-        // snapshot.forEach(data => {
-        //   this.menus.push(data.val());
-        // });
-        this.menus = snapshot.val();
-      });
-    // console.log(self.menus);
-
-    self.removeMenus = JSON.parse(localStorage.getItem("removeMenuKeys"));
   },
   mounted() {
     const self = this;
@@ -212,14 +169,14 @@ export default {
 
       let removeMenus = self.removeMenus;
 
-      /* click remove icon for remove menu*/
+      /* click remove icon for remove menu */
       // store
       //   .child(storeId)
       //   .child("menus")
       //   .child(key)
       //   .remove();
 
-      /* click remove icon to add  menu to localstorage for remove menu from firebase*/
+      /* click remove icon to add  menu to localstorage for remove menu from firebase */
       removeMenus.push(key);
 
       localStorage.setItem("removeMenuKeys", JSON.stringify(removeMenus));
@@ -271,90 +228,5 @@ h1 {
   height: 22px;
   line-height: 22px;
   margin-bottom: 50px;
-}
-
-.menus {
-  width: 77.6%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.17);
-  border-radius: 14px;
-  background: #ffffff;
-  padding: 30px 7px 0 17px;
-}
-
-ul {
-  width: 100%;
-  height: 307px;
-  // align-self: center;
-  overflow: auto;
-}
-
-li {
-  height: 50px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #e9e9e9;
-}
-.menu-align-area1 {
-  width: 100%;
-  display: flex;
-  align-items: flex-start;
-}
-.remove {
-  width: 19px;
-  height: 19px;
-  border-radius: 50%;
-  border: solid 1px #f75454;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  margin-right: 0;
-}
-
-.remove-inner {
-  width: 10px;
-  height: 2px;
-  background-color: #f75454;
-}
-
-.dishName {
-  height: 20px;
-  line-height: 20px;
-  display: inline-block;
-  margin-left: 13px;
-}
-
-.dishPrice {
-  height: 20px;
-  line-height: 20px;
-}
-
-.check-bar {
-  display: flex;
-  width: 100%;
-  height: 55px;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.add-menu {
-  color: #f8a654;
-  text-decoration: none;
-  margin-left: 25px;
-}
-
-.check {
-  margin-left: 30px;
-}
-
-.select-area {
-  margin-right: 35px;
-}
-
-p {
-  margin-top: 0;
 }
 </style>
